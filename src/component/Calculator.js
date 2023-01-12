a8a7d9a8816f4d6e67b071e660cfcfb5d48224bb
Dell Pc <mudasir9757@gmail.com>
2023-01-12 17:46:17 +0500

    Add Props and Event Handler to Component Calculat

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -1,38 +1,37 @@
 import React, { Component } from 'react';
+import calculate from '../logic/calculate';
+import KeyButtons from './keys';
 import './Calculator.css';
 
 export default class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
   }
 
+  EventHandler = (element) => {
+    const btnValue = element.keyTag;
+    const getCalcu = calculate(this.state, btnValue);
+    this.setState(getCalcu);
+  };
+
   render() {
+    const { total, operation, next } = this.state;
     return (
       <div className="calculator">
         <div className="display">
-          <input className="display-value" type="text" placeholder="0" />
+          <div className="display-value">
+            {total}
+            {operation}
+            {next}
+          </div>
         </div>
         <div className="inputs">
-          <button type="submit" className="input-btn" value="number">AC </button>
-          <button type="submit" className="input-btn" value="number">+/- </button>
-          <button type="submit" className="input-btn" value="number">% </button>
-          <button type="submit" className="input-btn btn-org" value="number">÷ </button>
-          <button type="submit" className="input-btn" value="number">7 </button>
-          <button type="submit" className="input-btn" value="number">8 </button>
-          <button type="submit" className="input-btn" value="number">9 </button>
-          <button type="submit" className="input-btn btn-org" value="number">x </button>
-          <button type="submit" className="input-btn" value="number">4 </button>
-          <button type="submit" className="input-btn" value="number">5 </button>
-          <button type="submit" className="input-btn" value="number">6 </button>
-          <button type="submit" className="input-btn btn-org" value="number">- </button>
-          <button type="submit" className="input-btn" value="number">1 </button>
-          <button type="submit" className="input-btn" value="number">2 </button>
-          <button type="submit" className="input-btn" value="number">3 </button>
-          <button type="submit" className="input-btn btn-org" value="number">+ </button>
-          <button type="submit" className="input-btn btn-zero" value="number">0 </button>
-          <button type="submit" className="input-btn" value="number">. </button>
-          <button type="submit" className="input-btn btn-org" value="number">= </button>
+          <KeyButtons onClickOperation={this.EventHandler} />
         </div>
       </div>
     );
