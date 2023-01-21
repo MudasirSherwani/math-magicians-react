67f5da92026689732dfa7f1578588853d8dea016
Dell Pc <mudasir9757@gmail.com>
2023-01-21 19:45:58 +0500

    build for render deploy

--- src/component/Home.js
+++ src/component/Home.js
@@ -4,7 +4,7 @@ import { LoremIpsum } from 'react-lorem-ipsum';
 
 const Home = () => (
   <div className="homepage">
-    <h2 className="heading">Welcome to our page!</h2>
+    <h2 className="heading">Welcome to Home page!</h2>
     <LoremIpsum p={2} />
   </div>
 );
